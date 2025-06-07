f48a96ce09ba4148db9df9fba0551884a710c417
aydrey282 <phyuthwalhnin@ogrenci.beykoz.edu.tr>
2025-06-07 17:26:26 +0300

    initial commit

---
+++ lib/l10n/app_localizations_fr.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get title => 'Navigation Anneau de Beykoz';
+
+  @override
+  String get home => 'Accueil';
+
+  @override
+  String get bus => 'Bus';
+
+  @override
+  String get settings => 'Paramètres';
+}
---
+++ lib/l10n/app_localizations_tr.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Turkish (`tr`).
+class AppLocalizationsTr extends AppLocalizations {
+  AppLocalizationsTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get title => 'Kampüsü Keşfedin';
+
+  @override
+  String get home => 'Ana Sayfa';
+
+  @override
+  String get bus => 'Otobüs';
+
+  @override
+  String get settings => 'Ayarlar';
+}
